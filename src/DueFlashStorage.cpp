58102782b4a9873c7bb17a9c471c6009abfd7bd7
Fede Raimondo <federaimondo@gmail.com>
2018-11-07 15:54:08 +0100

    Select FLASH in arduino due

--- src/DueFlashStorage.cpp
+++ src/DueFlashStorage.cpp
@@ -10,69 +10,70 @@ DueFlashStorage::DueFlashStorage() {
   }
 }
 
-byte DueFlashStorage::read(uint32_t address) {
-  return FLASH_START[address];
+byte DueFlashStorage::read(uint32_t offset, uint8_t flash_n) {
+    uint32_t flash_start;
+    flash_start = flash_n == 0 ? IFLASH0_ADDR : IFLASH1_ADDR;
+    return ((byte*) flash_start)[offset];
 }
-byte* DueFlashStorage::readAddress(uint32_t address) {
-  return FLASH_START+address;
-}
+// byte* DueFlashStorage::readAddress(uint32_t address) {
+//   return FLASH_START+address;
+// }
+//
+// boolean DueFlashStorage::write(uint32_t address, byte value) {
+//   uint32_t retCode;
+//   uint32_t byteLength = 1;
+//   byte *data;
+//
+//   retCode = flash_unlock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + byteLength - 1, 0, 0);
+//   if (retCode != FLASH_RC_OK) {
+//     _FLASH_DEBUG("Failed to unlock flash for write\n");
+//     return false;
+//   }
+//
+//   // write data
+//   retCode = flash_write((uint32_t)FLASH_START+address, &value, byteLength, 1);
+//   //retCode = flash_write((uint32_t)FLASH_START, data, byteLength, 1);
+//
+//   if (retCode != FLASH_RC_OK) {
+//     _FLASH_DEBUG("Flash write failed\n");
+//     return false;
+//   }
+//
+//   // Lock page
+//   retCode = flash_lock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + byteLength - 1, 0, 0);
+//   if (retCode != FLASH_RC_OK) {
+//     _FLASH_DEBUG("Failed to lock flash page\n");
+//     return false;
+//   }
+//   return true;
+// }
 
-boolean DueFlashStorage::write(uint32_t address, byte value) {
+boolean DueFlashStorage::write(uint32_t offset, byte *data, uint32_t dataLength, uint8_t flash_n) {
   uint32_t retCode;
-  uint32_t byteLength = 1;  
-  byte *data;
+  uint32_t flash_start, flash_size;
 
-  retCode = flash_unlock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + byteLength - 1, 0, 0);
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Failed to unlock flash for write\n");
-    return false;
-  }
+  flash_start = flash_n == 0 ? IFLASH0_ADDR : IFLASH1_ADDR;
+  flash_size = flash_n == 0 ? IFLASH0_SIZE : IFLASH1_SIZE;
 
-  // write data
-  retCode = flash_write((uint32_t)FLASH_START+address, &value, byteLength, 1);
-  //retCode = flash_write((uint32_t)FLASH_START, data, byteLength, 1);
-
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Flash write failed\n");
-    return false;
-  }
-
-  // Lock page
-  retCode = flash_lock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + byteLength - 1, 0, 0);
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Failed to lock flash page\n");
-    return false;
-  }
-  return true;
-}
-
-boolean DueFlashStorage::write(uint32_t address, byte *data, uint32_t dataLength) {
-  uint32_t retCode;
-
-  if ((uint32_t)FLASH_START+address < IFLASH1_ADDR) {
-    _FLASH_DEBUG("Flash write address too low\n");
-    return false;
-  }
-
-  if ((uint32_t)FLASH_START+address >= (IFLASH1_ADDR + IFLASH1_SIZE)) {
+  if (offset >= flash_size) {
     _FLASH_DEBUG("Flash write address too high\n");
     return false;
   }
 
-  if (((uint32_t)FLASH_START+address & 3) != 0) {
+  if ((flash_start+offset & 3) != 0) {
     _FLASH_DEBUG("Flash start address must be on four byte boundary\n");
     return false;
   }
 
   // Unlock page
-  retCode = flash_unlock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + dataLength - 1, 0, 0);
+  retCode = flash_unlock(flash_start+offset, flash_start+offset + dataLength - 1, 0, 0);
   if (retCode != FLASH_RC_OK) {
     _FLASH_DEBUG("Failed to unlock flash for write\n");
     return false;
   }
 
   // write data
-  retCode = flash_write((uint32_t)FLASH_START+address, data, dataLength, 1);
+  retCode = flash_write(flash_start+offset, data, dataLength, 1);
 
   if (retCode != FLASH_RC_OK) {
     _FLASH_DEBUG("Flash write failed\n");
@@ -80,57 +81,56 @@ boolean DueFlashStorage::write(uint32_t address, byte *data, uint32_t dataLength
   }
 
   // Lock page
-    retCode = flash_lock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + dataLength - 1, 0, 0);
+    retCode = flash_lock(flash_start+offset, flash_start+offset + dataLength - 1, 0, 0);
   if (retCode != FLASH_RC_OK) {
     _FLASH_DEBUG("Failed to lock flash page\n");
     return false;
   }
   return true;
 }
-
-boolean DueFlashStorage::write_unlocked(uint32_t address, byte value) {
-  uint32_t retCode;
-  uint32_t byteLength = 1;  
-  byte *data;
-
-  // write data
-  retCode = flash_write((uint32_t)FLASH_START+address, &value, byteLength, 1);
-  //retCode = flash_write((uint32_t)FLASH_START, data, byteLength, 1);
-
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Flash write failed\n");
-    return false;
-  }
-
-  return true;
-}
-
-boolean DueFlashStorage::write_unlocked(uint32_t address, byte *data, uint32_t dataLength) {
-  uint32_t retCode;
-
-  if ((uint32_t)FLASH_START+address < IFLASH1_ADDR) {
-    _FLASH_DEBUG("Flash write address too low\n");
-    return false;
-  }
-
-  if ((uint32_t)FLASH_START+address >= (IFLASH1_ADDR + IFLASH1_SIZE)) {
-    _FLASH_DEBUG("Flash write address too high\n");
-    return false;
-  }
-
-  if (((uint32_t)FLASH_START+address & 3) != 0) {
-    _FLASH_DEBUG("Flash start address must be on four byte boundary\n");
-    return false;
-  }
-
-  // write data
-  retCode = flash_write((uint32_t)FLASH_START+address, data, dataLength, 1);
-
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Flash write failed\n");
-    return false;
-  }
-
-  return true;
-}
-
+//
+// boolean DueFlashStorage::write_unlocked(uint32_t address, byte value) {
+//   uint32_t retCode;
+//   uint32_t byteLength = 1;
+//   byte *data;
+//
+//   // write data
+//   retCode = flash_write((uint32_t)FLASH_START+address, &value, byteLength, 1);
+//   //retCode = flash_write((uint32_t)FLASH_START, data, byteLength, 1);
+//
+//   if (retCode != FLASH_RC_OK) {
+//     _FLASH_DEBUG("Flash write failed\n");
+//     return false;
+//   }
+//
+//   return true;
+// }
+//
+// boolean DueFlashStorage::write_unlocked(uint32_t address, byte *data, uint32_t dataLength) {
+//   uint32_t retCode;
+//
+//   if ((uint32_t)FLASH_START+address < IFLASH1_ADDR) {
+//     _FLASH_DEBUG("Flash write address too low\n");
+//     return false;
+//   }
+//
+//   if ((uint32_t)FLASH_START+address >= (IFLASH1_ADDR + IFLASH1_SIZE)) {
+//     _FLASH_DEBUG("Flash write address too high\n");
+//     return false;
+//   }
+//
+//   if (((uint32_t)FLASH_START+address & 3) != 0) {
+//     _FLASH_DEBUG("Flash start address must be on four byte boundary\n");
+//     return false;
+//   }
+//
+//   // write data
+//   retCode = flash_write((uint32_t)FLASH_START+address, data, dataLength, 1);
+//
+//   if (retCode != FLASH_RC_OK) {
+//     _FLASH_DEBUG("Flash write failed\n");
+//     return false;
+//   }
+//
+//   return true;
+// }
--- src/DueFlashStorage.h
+++ src/DueFlashStorage.h
@@ -1,10 +1,10 @@
-/* 
+/*
 DueFlashStorage saves non-volatile data for Arduino Due.
 The library is made to be similar to EEPROM library
 Uses flash block 1 per default.
 
 Note: uploading new software will erase all flash so data written to flash
-using this library will not survive a new software upload. 
+using this library will not survive a new software upload.
 
 Inspiration from Pansenti at https://github.com/Pansenti/DueFlash
 Rewritten and modified by Sebastian Nilsson
@@ -22,7 +22,7 @@ Rewritten and modified by Sebastian Nilsson
 #define DATA_LENGTH   ((IFLASH1_PAGE_SIZE/sizeof(byte))*4)
 
 // choose a start address that's offset to show that it doesn't have to be on a page boundary
-#define  FLASH_START  ((byte *)IFLASH1_ADDR)
+// #define  FLASH_START  ((byte *)IFLASH0_ADDR)
 
 //  FLASH_DEBUG can be enabled to get debugging information displayed.
 #define FLASH_DEBUG
@@ -37,18 +37,18 @@ Rewritten and modified by Sebastian Nilsson
 class DueFlashStorage {
 public:
   DueFlashStorage();
-  
+
   // write() writes the specified amount of data into flash.
   // flashStart is the address in memory where the write should start
   // data is a pointer to the data to be written
   // dataLength is length of data in bytes
-  
-	byte read(uint32_t address);
-	byte* readAddress(uint32_t address);
-	boolean write(uint32_t address, byte value);
-	boolean write(uint32_t address, byte *data, uint32_t dataLength);
-	boolean write_unlocked(uint32_t address, byte value);
-	boolean write_unlocked(uint32_t address, byte *data, uint32_t dataLength);
+
+	byte read(uint32_t offset, uint8_t flash_n);
+	// byte* readAddress(uint32_t address);
+	// boolean write(uint32_t address, byte value);
+	boolean write(uint32_t offset, byte *data, uint32_t dataLength, uint8_t flash_n);
+	// boolean write_unlocked(uint32_t address, byte value);
+	// boolean write_unlocked(uint32_t address, byte *data, uint32_t dataLength);
 };
 
 #endif
